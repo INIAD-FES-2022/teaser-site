ca3adb547ea78f93256e0de4e08079779244b26d
littleswampman <littleswampman@gmail.com>
2022-07-26 18:55:20 +0900

    fix _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import "../styles/globals.css";
+import { NextComponentType, NextPageContext } from "next";
+import { ReactNode, FC } from "react";
 import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
-}
+import "../styles/globals.css";
 
+type Props<P = Record<string, unknown>> = {
+    Component: NextComponentType<NextPageContext, unknown, P>;
+    pageProps: ReactNode;
+};
+
+const MyApp: FC<Props> = ({ Component, pageProps }) => (
+    <Component>{pageProps}</Component>
+);
+
+// eslint-disable-next-line import/no-default-export
 export default MyApp;
