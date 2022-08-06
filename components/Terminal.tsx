da545f61981f0e001a94b9583a3f04a0d2611abf
littleswampman <littleswampman@gmail.com>
2022-08-06 19:28:52 +0900

    delete and minimize functions added to Terminal

--- components/Terminal.tsx
+++ components/Terminal.tsx
@@ -1,41 +1,111 @@
-import React, { FC, ReactNode } from "react";
+import React, { FC, FunctionComponent,ReactNode, useState } from "react";
 import styles from "../styles/Terminal.module.scss";
 
 type Props = {
     children: ReactNode;
+    size?: "Small" | "Medium" | "Large";
+    isDeletable?: boolean;
+    isMinimizable?: boolean;
 };
 
-export const Terminal: FC<Props> = ({ children }) => {
-    const hoge = 0;
+export const Terminal: FC<Props> = ({
+    children,
+    size="Medium",
+    isDeletable =false,
+    isMinimizable=false,
+}) => {
+    // ターミナル風の何かに表示されている様な見た目にする
+    // sizeは指定がなければ"Medium"になる
+    // TODO sizeの指定で大きさを調整出来るようにしたい
+    // isDeletable(default:false)がtrueなら×をクリックすることでこのコンポーネントは削除される
+    // isMinimize(default:false)がtrueなら_をクリックすると最小化?っぽい見た目になって、その状態で□をクリックすると元に戻る
+
+    const [isDeleted, setIsDeleted] = useState(false);
+    const [isMinimize, setIsMinimize] = useState(false);
 
     return (
-        <div
-            className={`${styles.terminal} relative flex flex-col bg-slate-800`}
-        >
-            <div className="terminal-top flex h-6 w-full flex-row-reverse items-center">
-                <div className="terminal-top--x relative flex h-full w-8 items-center justify-center bg-red-500">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
+        // 空のfragment(<></>)を使うなって怒られるけれど、
+        // eslint-disable-next-line react/jsx-no-useless-fragment
+        <>
+            {!isDeleted && (
+                <div className={`${styles.terminal} flex flex-col`}>
+                    <div
+                        className={`${styles.terminalTop} ${
+                            isMinimize ? "rounded-md" : "rounded-t-md"
+                        } flex h-6 w-full flex-row-reverse items-center`}
                     >
-                        <path
-                            fill="none"
-                            stroke="#FFF"
-                            strokeWidth={2}
-                            d="M3,3 L21,21 M3,21 L21,3"
-                        />
-                    </svg>
+                        <button
+                            type="button"
+                            className={`${styles.terminalTopX} flex h-full w-8 items-center justify-center`}
+                            onClick={() => {
+                                if (isDeletable) {
+                                    setIsDeleted(true);
+                                }
+                            }}
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                            >
+                                <path
+                                    fill="none"
+                                    stroke="#FFF"
+                                    strokeWidth={2}
+                                    d="M3,3 L21,21 M3,21 L21,3"
+                                />
+                            </svg>
+                        </button>
+                        <button
+                            type="button"
+                            className="terminal-top--□ flex h-full w-8 items-center justify-center"
+                            onClick={() => {
+                                if (isMinimizable) {
+                                    setIsMinimize(false);
+                                }
+                            }}
+                        >
+                            <div className="m-1 border-2 border-solid border-white py-1 px-1.5" />
+                        </button>
+                        <button
+                            type="button"
+                            className="terminal-top--_ flex h-full w-8 flex-col-reverse items-center"
+                            onClick={() => {
+                                if (isMinimizable) {
+                                    setIsMinimize(true);
+                                }
+                            }}
+                        >
+                            {/* <div className="m-2 border-b-2 border-solid border-white" /> */}
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                            >
+                                <line
+                                    x1={4}
+                                    y1={14}
+                                    x2={20}
+                                    y2={14}
+                                    stroke="#FFF"
+                                    strokeWidth={2}
+                                />
+                                {/* <path
+                                    fill="none"
+                                    stroke="#FFF"
+                                    strokeWidth={2}
+                                    d="M6,12 L18,12"
+                                /> */}
+                            </svg>
+                        </button>
+                    </div>
+                    {!isMinimize && (
+                        <div className="terminal-content p-1">{children}</div>
+                    )}
                 </div>
-                <div className="terminal-top--□ flex h-full w-8 items-center justify-center bg-slate-400">
-                    <div className="m-1 border-2 border-solid border-white py-1 px-2" />
-                </div>
-                <div className="terminal-top--_ flex h-full w-8 flex-col-reverse bg-slate-400">
-                    <div className="m-2 border-b-2 border-solid border-white" />
-                </div>
-            </div>
-            <div className="terminal-content p-4">{children}</div>
-        </div>
+            )}
+        </>
     );
 };
