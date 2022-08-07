d35d2488b543380c7dfd37469f08fb641e8b327d
littleswampman <littleswampman@gmail.com>
2022-08-08 00:57:52 +0900

    add MainVisual & Terminals and change index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.scss";
 import { Terminal } from "../components/Terminal";
+import { MainVisual } from "../components/MainVisual";
 
 const Home: NextPage = () => (
     <div className={styles.container}>
@@ -13,70 +14,25 @@ const Home: NextPage = () => (
         </Head>
 
         <main className={styles.main}>
-            <h1 className={styles.title}>
-                Welcome to <a href="https://nextjs.org">Next.js!</a>
-            </h1>
-
-            <p className={styles.description}>
-                Get started by editing{" "}
-                <code className={styles.code}>pages/index.tsx</code>
-            </p>
-
-            <div className={styles.grid}>
-                <a href="https://nextjs.org/docs" className={styles.card}>
-                    <h2>Documentation &rarr;</h2>
-                    <p>
-                        Find in-depth information about Next.js features and
-                        API.
-                    </p>
-                </a>
-
-                <a href="https://nextjs.org/learn" className={styles.card}>
-                    <h2>Learn &rarr;</h2>
-                    <p>
-                        Learn about Next.js in an interactive course with
-                        quizzes!
-                    </p>
-                </a>
-
-                <a
-                    href="https://github.com/vercel/next.js/tree/canary/examples"
-                    className={styles.card}
-                >
-                    <h2>Examples &rarr;</h2>
-                    <p>
-                        Discover and deploy boilerplate example Next.js
-                        projects.
-                    </p>
-                </a>
-
-                <a
-                    href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                    className={styles.card}
-                >
-                    <h2>Deploy &rarr;</h2>
-                    <p>
-                        Instantly deploy your Next.js site to a public URL with
-                        Vercel.
-                    </p>
-                </a>
-            </div>
-            <div className="flex flex-col gap-4">
-                <Terminal isDeletable>
-                    <div className="bg-orange-600 p-6 ">
-                        <p>deletable terminal</p>
-                    </div>
-                </Terminal>
-                <Terminal isMinimizable>
-                    <div className="bg-orange-600 p-6 ">
-                        <p>minimizable terminal</p>
-                    </div>
-                </Terminal>
-                <Terminal>
-                    <div className="bg-orange-600 p-6 ">
-                        <p>normal terminal</p>
-                    </div>
-                </Terminal>
+            <div>
+                <MainVisual />
+                <div id="terminals" className="flex flex-col gap-4">
+                    <Terminal isDeletable>
+                        <div className="bg-orange-600 p-6 ">
+                            <p>deletable terminal</p>
+                        </div>
+                    </Terminal>
+                    <Terminal isMinimizable>
+                        <div className="bg-orange-600 p-6 ">
+                            <p>minimizable terminal</p>
+                        </div>
+                    </Terminal>
+                    <Terminal>
+                        <div className="bg-orange-600 p-6 ">
+                            <p>normal terminal</p>
+                        </div>
+                    </Terminal>
+                </div>
             </div>
         </main>
 
