3280eba3045f2b8c3068861873c81af0a7f096c4
littleswampman <littleswampma@gmail.com>
2022-08-09 15:15:07 +0900

    change to store the width of the element in the variable "-loop-width"
    --loop-widthに要素の横幅を入力するようにした
    これでCSSでの操作がやりやすくなると思いたい

--- components/MainVisual.tsx
+++ components/MainVisual.tsx
@@ -1,52 +1,67 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "../styles/MainVisual.module.scss";
+
+// import { useRect } from "../service/useRect";
 
 export const MainVisual = () => {
     const loopingClass = Object.keys(styles)
         .filter((className) => className.match(/looping\d/))
         .sort();
-    const ref = useRef<HTMLDivElement>(null);
+    const ref0 = useRef<HTMLDivElement | null>(null);
+    const ref1 = useRef<HTMLDivElement | null>(null);
+    const ref2 = useRef<HTMLDivElement | null>(null);
+    const ref3 = useRef<HTMLDivElement | null>(null);
+    const ref4 = useRef<HTMLDivElement | null>(null);
+    const ref5 = useRef<HTMLDivElement | null>(null);
+    const ref6 = useRef<HTMLDivElement | null>(null);
+    const ref7 = useRef<HTMLDivElement | null>(null);
 
-    const handleOnClick = () => {
-        if (ref.current !== null) {
-            console.log(
-                getComputedStyle(ref.current).getPropertyValue("height"),
-            );
-        }
-    };
-
-    return (
-        <>
-            {/* TODO クラスのあて方を整理する */}
-            <div ref={ref} className={`${styles.container} h-screen w-screen`}>
-                {
-                    <div className={`${styles.loopingContainer}`}>
-                        {[...Array(8).keys()].map((numLoop) => (
-                            // 8行
-                            <div
-                                // FIXME 完全に型安全なやり方は何なのだろう
-                                // これでおかしくなることはないとは思うけれど
-                                // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-                                className={`${styles[loopingClass[numLoop]]} ${
-                                    styles.looping
-                                } flex flex-nowrap gap-4`}
-                                key={numLoop}
-                            >
-                                {[...Array(24).keys()].map((numLoopInside) => (
-                                    // 24列
-                                    <div className="flex" key={numLoopInside}>
-                                        <p>INIAD-</p>
-                                        <p>FES</p>
-                                    </div>
-                                ))}
-                            </div>
-                        ))}
-                    </div>
+    const refList = [ref0, ref1, ref2, ref3, ref4, ref5, ref6, ref7];
+    const [widthList, setWidthList] = useState<number[]>([]);
+    useEffect(() => {
+        setWidthList(
+            refList.map((ref) => {
+                if (ref.current !== null) {
+                    return Number(ref.current.clientWidth);
                 }
-            </div>
-            <button type="button" onClick={handleOnClick}>
-                button
-            </button>
-        </>
+
+                return -1;
+            }),
+        );
+        console.log(widthList);
+    }, [refList[0].current]);
+
+    const loopDataList = [...Array(8).keys()].map((i) => ({
+        style: {
+            "--loop-width": `${widthList[i] ?? 1000}px`,
+        } as React.CSSProperties,
+        ref: refList[i],
+    }));
+
+    // {/* TODO クラスのあて方を整理する */}
+    return (
+        <div className={`${styles.container} h-screen w-screen`}>
+            {loopDataList.map(({ style, ref }, i) => (
+                <div
+                    ref={ref}
+                    // FIXME 完全に型安全なやり方は何なのだろう
+                    // これでおかしくなることはないとは思うけれど
+                    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+                    className={`${styles[loopingClass[i]]} ${
+                        styles.looping
+                    } flex flex-nowrap gap-4`}
+                    key={i}
+                    style={style}
+                >
+                    {[...Array(24).keys()].map((numLoopInside) => (
+                        // 24列
+                        <div className="flex" key={numLoopInside}>
+                            <p>INIAD-</p>
+                            <p>FES</p>
+                        </div>
+                    ))}
+                </div>
+            ))}
+        </div>
     );
 };
