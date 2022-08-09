73218454f9b80a09bddfc55557581b81c40a2e3f
littleswampman <littleswampman@users.noreply.github.com>
2022-08-09 14:48:52 +0000

    deploy: 80fa990830d9315fad49a5fea4d6306682e1a023

---
+++ _next/static/TlNR_lkViB_LKkhKclJS4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/50708ba00302bdac.css","static/chunks/pages/index-5b50df701330870c.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
