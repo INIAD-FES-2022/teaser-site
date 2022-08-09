e37db32dede0f2a5925c76a268b157aee7a54828
littleswampman <littleswampman@gmail.com>
2022-08-09 23:40:14 +0900

    fix next.config.js for "next export"

--- next.config.js
+++ next.config.js
@@ -6,6 +6,12 @@ const nextConfig = {
     // GitHub pagesの為に追加
     basePath: process.env.GITHUB_ACTIONS && "/teaser-site",
     trailingSlash: true, // "/about"のようなURLを"/about/"へリダイレクトする(デフォルトは逆)
-};
 
+    // "next export" で制的サイトを出力する為に画像の最適化を切る
+    experimental: {
+        images: {
+            unoptimized: true,
+        },
+    },
+};
 module.exports = nextConfig;
