ae9bed21f7e17421579d34fd3931043b4d5c500e
littleswampman <littleswampman@gmail.com>
2022-08-09 22:40:04 +0900

    fix MainVisual.tsx finally

--- components/MainVisual.tsx
+++ components/MainVisual.tsx
@@ -37,7 +37,7 @@ export const MainVisual = () => {
         ref: refList[i],
     }));
 
-    // {/* TODO クラスのあて方を整理する */}
+    // TODO クラスのあて方を整理する
     return (
         <div className={`${styles.outerContainer} relative h-screen w-screen`}>
             {[...Array(4).keys()].map((h) => (
@@ -46,38 +46,38 @@ export const MainVisual = () => {
                 // 流石にここまですれば、拡大縮小でデザインが崩れることは変なことをしない限り起こらないと信じたい
 
                 <div className={`${styles.container}`} key={h}>
-                    {loopDataList.map(({ style, ref }, i) => (
-                        <div
-                            key={i}
-                            // FIXME 完全に型安全なやり方は何なのだろう
-                            // これでおかしくなることはないとは思うけれど
-                            // NOTE 解決方法を模索する中で、https://vanilla-extract.style/ を見つけた、これでCSS周りを書き直せばなんとかなる気もする
-                            // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-                            className={`${styles[loopingClass[i]]} ${
-                                styles.loopingContainer
-                            } flex gap-4`}
-                        >
-                            {[...Array(2).keys()].map((j) => (
-                                // 2回繰り返しているだけ
-                                // アニメーションのことを考えると、1つでは連続して流れている様には見えないだろうから、2つに増やして上手いこと動かしてなんとかする
-                                // refは上書きされて1つ目のものではなく2つ目のloopElementsだけにあたっているけれど、横幅は同じ筈なので問題はないと思う
-                                <div
-                                    ref={ref}
-                                    className={`${styles.looping} flex flex-nowrap gap-4`}
-                                    key={`${i}-${j}`}
-                                    style={style}
-                                >
-                                    {[...Array(24).keys()].map((num) => (
-                                        // 24列
-                                        <div className="flex" key={num}>
-                                            <p>INIAD-</p>
-                                            <p>FES</p>
-                                        </div>
-                                    ))}
-                                </div>
-                            ))}
-                        </div>
-                    ))}
+                    {loopDataList.map(({ style, ref }, i) => {
+                        const loopingClassName = styles[
+                            loopingClass[i] as keyof typeof styles
+                        ] as string;
+
+                        return (
+                            <div
+                                key={i}
+                                className={`${loopingClassName} ${styles.loopingContainer} flex gap-4`}
+                            >
+                                {[...Array(2).keys()].map((j) => (
+                                    // 2回繰り返しているだけ
+                                    // アニメーションのことを考えると、1つでは連続して流れている様には見えないだろうから、2つに増やして上手いこと動かしてなんとかする
+                                    // refは上書きされて1つ目のものではなく2つ目のloopElementsだけにあたっているけれど、横幅は同じ筈なので問題はないと思う
+                                    <div
+                                        ref={ref}
+                                        className={`${styles.looping} flex flex-nowrap gap-4`}
+                                        key={`${i}-${j}`}
+                                        style={style}
+                                    >
+                                        {[...Array(24).keys()].map((num) => (
+                                            // 24列
+                                            <div className="flex" key={num}>
+                                                <p>INIAD-</p>
+                                                <p>FES</p>
+                                            </div>
+                                        ))}
+                                    </div>
+                                ))}
+                            </div>
+                        );
+                    })}
                 </div>
             ))}
         </div>
