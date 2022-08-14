a54d9a80c6534e2ef040613a250d1f78d027ef8a
littleswampman <littleswampman@users.noreply.github.com>
2022-08-14 12:14:33 +0000

    deploy: 34c0da863f3decd5b0fae6898f996d841c430dbd

--- _next/static/RruBdG4KWzREjMTldLJBo/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/8dbdd48a224fa6b4.css","static/chunks/pages/index-9a639870efd467b8.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
